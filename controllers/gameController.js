b52b7db0c9c663016b406c8df53e84bb43755eac
Jeaneth Farmer <xaid27@gmail.com>
2013-11-08 23:14:05 -0600

    create endpoint to create users

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,6 +1,19 @@
+var mongoose   = require('mongoose');
+
+var userSchema = require('../models/user');
+var User     = mongoose.model('user', userSchema);
+
+var onError = function(err){
+  log(err)
+};
+
 module.exports = {
   createUser: function(req, res){
-    res.send("testing");
+    var user = req.body;
+    
+    var newUser = new User(user);
+    newUser.save(onError);
+    res.send(newUser);
   }
 }
 
