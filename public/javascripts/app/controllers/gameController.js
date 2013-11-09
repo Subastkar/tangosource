cd4ee98074f08f5f69fb6f62f1875ebcad750142
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-11-08 20:10:07 -0600

    Added basi crafty area

--- public/javascripts/app/controllers/gameController.js
+++ public/javascripts/app/controllers/gameController.js
@@ -2,6 +2,10 @@ ZombieWorld.Controller.gameController = {
 
   init: function(){
     ZombieWorld.Controller.socketController.init();
+    
+    //This should be changed with the specifics of the maps once the config is done
+    Crafty.init(1040,540, 'game-area')
+    Crafty.background('rgb(56,208,135)');
   }
 
 };
