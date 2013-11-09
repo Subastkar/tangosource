60b548f4e74db5c19cafc745866c006d7f79c777
Narciso Guillen <narciso.guillen@tangosource.com>
2013-11-08 21:40:44 -0800

    Build player entitie

---
+++ public/javascripts/app/entities/player.js
@@ -0,0 +1,14 @@
+ZombieWorld.Entities.player = function(player){
+
+  return Crafty.e('Player, ' + player.type )
+      .attr({
+        x: player.x,
+        y: player.y
+      })
+      .requires('Keyboard')
+      .animate("walk_left",  0, 1, 3)
+      .animate("walk_right", 0, 2, 3)
+      .animate("walk_up",    0, 3, 3)
+      .animate("walk_down",  0, 0, 3);
+
+};
