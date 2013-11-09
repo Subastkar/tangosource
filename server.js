d5ea4489622f21b5a9759e336c2b7980de76d6b2
Narciso Guillen <narciso.guillen@tangosource.com>
2013-11-08 22:11:36 -0800

    if run as root, downgrade to the owner of this file

--- server.js
+++ server.js
@@ -43,6 +43,15 @@ if('development' === app.get('env')){
 require('./routes')(app);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0) {
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) { return console.error(err); }
+      process.setuid(stats.uid);
+    });
+  }
+
   console.log('Express server listening on port ' + app.get('port'));
 });
 
