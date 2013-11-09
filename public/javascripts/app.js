54f4c40b6297e66d90c212fb2a47f7e839d5738e
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-11-09 08:11:33 -0600

    Added other players and positions to load

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,11 +8,17 @@ var ZombieWorld = {
   Players:     {},
 
   Sprites: {
-
     player1: Crafty.sprite(40, "/images/player1.png", {
       player1: [0,0]
     }),
 
+    player2: Crafty.sprite(40, "/images/player1.png", {
+      player2: [0,0]
+    }),
+
+    player3: Crafty.sprite(40, "/images/player1.png", {
+      player3: [0,0]
+    })
   },
 
   onError: function(error){
