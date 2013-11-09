2ecef0dcc7bfecfd9b3ab88270c8a6044be2fcc0
Jeaneth Farmer <xaid27@gmail.com>
2013-11-09 00:33:20 -0600

    sending player configuration

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,10 +1,14 @@
 var mongoose   = require('mongoose');
+var config     = require('../config/game');
 
-var roomSchema = require('../models/room');
-var userSchema = require('../models/user');
+var roomSchema   = require('../models/room');
+var userSchema   = require('../models/user');
+var playerSchema = require('../models/player');
 
-var User     = mongoose.model('user', userSchema);
-var Room     = mongoose.model('room', roomSchema);
+var User   = mongoose.model('user', userSchema);
+var Room   = mongoose.model('room', roomSchema);
+var Player = mongoose.model('player', playerSchema);
+
 
 var onError = function(err){
   log(err)
@@ -13,9 +17,10 @@ var onError = function(err){
 module.exports = {
   createUser: function(req, res){
     var user = req.body;
-    
-    var newUser = new User(user);
+
+    var newUser = new User(_.extend(user, { alive: true, waiting: false}));
     newUser.save(onError);
+
 
     //Search for a room available
     Room.findOne({$where: 'this.players.length < 4'}).exec(function(err, currentRoom){
@@ -27,11 +32,27 @@ module.exports = {
         level: 1
       });
 
-
-      log(newUser);
       room.players.push(newUser._id);
       room.save(onError);
-      res.send({user: newUser, room: room});
+
+      var data = config['level' + room.level].position1;
+
+      User.findOneAndUpdate({_id: newUser.id}, data, function(err, userUpdated){
+
+        //Get player configuration
+        Player.findOne({name: user.type }, function(err, playerConf){
+          if(err){ res.send(400, err); }
+
+          var response = {
+            user: userUpdated,
+            conf: playerConf,
+            room: room
+          }
+          res.send(response);
+        });
+
+      });
+
     });
 
   }
