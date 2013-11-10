18f1728874798e8738d23007a644f4af20724060
Jeaneth Farmer <xaid27@gmail.com>
2013-11-10 01:26:11 -0800

    zombies can go through the wails

--- public/javascripts/app/components/zombie.js
+++ public/javascripts/app/components/zombie.js
@@ -1,5 +1,6 @@
 ZombieWorld.Components.zombie = Crafty.c('Zombie', {
   init: function(){
-    this.addComponent('2D, Canvas, SpriteAnimation, Tween, Mouse');
+    this.addComponent('2D, Collision, Canvas, SpriteAnimation, Tween, Mouse')
+    .collision([0,0],[40,0],[40,40],[0,40]);
   }
 });
--- public/javascripts/app/controllers/zombieController.js
+++ public/javascripts/app/controllers/zombieController.js
@@ -15,24 +15,52 @@ ZombieWorld.Controller.zombieController = {
     var zombie = Zombie.Entity;
     var destiny = opts.destiny;
 
+    if(!zombie.__c.Tween){ zombie.addComponent('Tween'); }
 
     if(zombie.isPlaying()){ 
       zombie.stop();}
 
-    var animation;
-    var difX = zombie.x - destiny.x;
-    var difY = zombie.y - destiny.y;
+      var animation;
+      var difX = zombie.x - destiny.x;
+      var difY = zombie.y - destiny.y;
 
-    if(Math.abs(difX) > Math.abs(difY)){
-      animation = zombie.x > destiny.x ? 'walk_left' : 'walk_right' ;
-    }else{
-      animation = zombie.y > destiny.y ? 'walk_up' : 'walk_down' ;
-    }
-    zombie.animate(animation, 20, -1);
+      if(Math.abs(difX) > Math.abs(difY)){
+        animation = zombie.x > destiny.x ? 'walk_left' : 'walk_right' ;
+      }else{
+        animation = zombie.y > destiny.y ? 'walk_up' : 'walk_down' ;
+      }
+      zombie.animate(animation, 20, -1);
 
-    zombie.tween({speed: 1, x: destiny.x , y: destiny.y}, 250).bind('TweenEnd', function(){
-      zombie.stop();
-    });
+      zombie.tween({speed: 1, x: destiny.x , y: destiny.y}, 250)
+      .onHit('Obstacle', function(e){
+
+        zombie.stop();
+
+        if (this._movement) {
+          this.x -= this._movement.x;
+          this.y -= this._movement.y;
+        }
+
+        if(e[0].obj.x > this.x){
+          this.x -=1;
+        }
+
+        if(e[0].obj.x < this.x){
+          this.x +=1;
+        }
+
+        if(e[0].obj.y > this.y){
+          this.y -=1;
+        }
+
+        if(e[0].obj.y < this.y){
+          this.y +=1;
+        }
+
+        this.removeComponent('Tween');
+      }).bind('TweenEnd', function(){
+        zombie.stop();
+      });
   }
 
 };
