246a6dc01724cfd6e405aa2b328dc7a3172cfd55
Narciso Guillen <narciso.guillen@tangosource.com>
2013-11-09 17:07:47 -0800

    Liston wen user moves

--- public/javascripts/app/entities/player.js
+++ public/javascripts/app/entities/player.js
@@ -9,6 +9,9 @@ ZombieWorld.Entities.player = function(player){
       .animate("walk_left",  0, 1, 3)
       .animate("walk_right", 0, 2, 3)
       .animate("walk_up",    0, 3, 3)
-      .animate("walk_down",  0, 0, 3);
+      .animate("walk_down",  0, 0, 3)
+      .bind('Move', function(from){
+        console.log(from);
+      });
 
 };
