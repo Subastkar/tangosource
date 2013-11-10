bb2f33db66d8635d2750b14a9a3437ae1a04891a
Jeaneth Farmer <xaid27@gmail.com>
2013-11-10 11:20:38 -0800

    change validation to login

--- public/javascripts/app/components/zombie.js
+++ public/javascripts/app/components/zombie.js
@@ -3,6 +3,6 @@ ZombieWorld.Components.zombie = Crafty.c('Zombie', {
     this._life = 0;
     this._speed = 0;
     this.addComponent('2D, Collision, Canvas, SpriteAnimation, Tween, Mouse')
-    .collision([0,0],[40,0],[40,40],[0,40]);
+    .collision([0,-20],[30,-20],[30,30],[0,30]);
   }
 });
--- public/javascripts/app/controllers/loginController.js
+++ public/javascripts/app/controllers/loginController.js
@@ -7,18 +7,16 @@ $(function(){
       player: $('input:radio[name="player"]:checked').val()
     };
 
-    if(user.username && user.player){
-      $.ajax({
-        method: 'POST',
-        url: 'user/create',
-        data: user
-      }).done(function(res){
-        localStorage.setItem('user', JSON.stringify(res.user));
-        localStorage.setItem('room', JSON.stringify(res.room));
-        window.location.assign('/game');
-      });
-    }else{
-      alert('Please enter a username and pick a player');
-    }
+    if(!user.username || !user.player){return alert('Please enter a username and pick a player');}
+
+    $.ajax({
+      method: 'POST',
+      url: 'user/create',
+      data: user
+    }).done(function(res){
+      localStorage.setItem('user', JSON.stringify(res.user));
+      localStorage.setItem('room', JSON.stringify(res.room));
+      window.location.assign('/game');
+    });
   });
 });
